10f31b40d7eb44d722b9455f68ac8bb432c7e964
faherngeit <valeriy.tashchilin@hey.com>
2021-08-10 15:28:19 +0500

    Start updating process

---
+++ local.py
@@ -0,0 +1,32 @@
+from io import StringIO
+from lxml import etree
+
+
+with open('XSD/SCL.xsd') as file:
+    xml_doc = etree.parse(file)
+    xml_scheme = etree.XMLSchema(xml_doc)
+
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    file_name = uploaded_file.name
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    string_data = stringio.read()
+
+    try:
+        st.markdown('Document parsing')
+        xml_file = etree.fromstring(uploaded_file.getvalue())
+        st.text('Parsing is Ok!')
+        st.markdown('Document verification')
+        try:
+            xml_scheme.assertValid(xml_file)
+            st.text('Verification is Ok!')
+
+        except etree.DocumentInvalid as err:
+            st.text(err.error_log)
+
+    except etree.XMLSyntaxError as err:
+        st.text(err.error_log)
+
+if __name__ == '__main__':
+    pass
